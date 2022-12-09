ffe3b00f8c24088ae503ef5b67d218a34060354e
Samir Cambraia <cambraia_samir@hotmail.com>
2022-12-09 17:49:06 -0300

    Add api buscar cep

---
+++ flutter-cep/fluuter_cep/lib/home.dart
@@ -0,0 +1,77 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:http/http.dart' as http;
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String resultado = "Seu cep irá aparecer aqui";
+
+  TextEditingController txtcep = TextEditingController();
+
+  void buscaCep() async {
+    String cep = txtcep.text;
+
+    String url = "https://viacep.com.br/ws/$cep/json/";
+
+    http.Response response;
+
+    response = await http.get(url);
+
+    print("Resposta" + response.body);
+    print("StatusCode: " + response.statusCode.toString());
+
+    Map<String, dynamic> dados = json.decode(response.body);
+
+    String logradouro = dados["logradouro"];
+    String complemento = dados["complemento"];
+    String bairro = dados["bairro"];
+    String localidade = dados["localidade"];
+
+    String endereco =
+        " O Endereço é:  + $logradouro, $complemento, $bairro, $localidade";
+    setState(() {
+      resultado = endereco;
+    });
+  } //end buscaCep()
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consulmo de API"),
+        backgroundColor: Colors.amber,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(60),
+        child: Center(
+          child: Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: <Widget>[
+              TextField(
+                controller: txtcep,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: "Digite o cep: "),
+                style: TextStyle(fontSize: 16, color: Colors.lightBlueAccent),
+              ),
+              ElevatedButton(
+                onPressed: buscaCep,
+                child: Text("Consultar"),
+              ),
+              Text(resultado),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
